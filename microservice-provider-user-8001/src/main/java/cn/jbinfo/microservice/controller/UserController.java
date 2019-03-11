f374f02e972592ba4595a0048fca951dc03ebf61
chenghuasui <292812381@qq.com>
2019-03-11 14:49:18 +0800

    再打印一条日志

--- microservice-provider-user-8001/src/main/java/cn/jbinfo/microservice/controller/UserController.java
+++ microservice-provider-user-8001/src/main/java/cn/jbinfo/microservice/controller/UserController.java
@@ -44,6 +44,7 @@ public class UserController
 	@RequestMapping(value = "/user/discovery", method = RequestMethod.GET)
     public Object discovery()
     {
+    	System.out.println("123123123123123123");
         List<String> list = client.getServices();
         List<ServiceInstance> srvList = client.getInstances("MICROSERVICE-USER");
         for (ServiceInstance element : srvList) {
